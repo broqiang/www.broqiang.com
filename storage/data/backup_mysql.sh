a6fd4bc3e55061edb98f31b21cce859b225e152d
Bro Qiang <broqiang@qq.com>
2018-03-12 20:23:30 +0800

    修改备份脚本

--- storage/data/backup_mysql.sh
+++ storage/data/backup_mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 
-mysqldump -uwww -p --databases "www_broqiang_com" > www_broqiang_com_$(date +%Y-%m-%d).sql
+mysqldump -uwww -p -h 127.0.0.1 --databases "www_broqiang_com" > www_broqiang_com_$(date +%Y-%m-%d).sql
