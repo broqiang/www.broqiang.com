9635c74307d3cdd5e1789ebdfc2e5803abd1b538
Bro Qiang <broqiang@qq.com>
2018-03-12 21:00:43 +0800

    修改 404 提示页面

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,15 +4,19 @@
 <div class="container">
     <div class="card">
         <div class="card-body">
-            <h1 class="text-center text-warning"> 404 </h1>
+            <center>
+                <h1 class="text-warning"> 404 </h1>
 
-            <div class="dropdown-divider"></div>
-            
-            <h3 class="text-center text-muted">
-                 访问的地址不存在 ! 
-            </h3>
-            
-            <p>{{ $exception->getMessage() }}</p>
+                <div class="dropdown-divider"></div>
+                
+                <h3 class="text-muted">
+                     访问的地址不存在 ! 
+                </h3>
+
+                <p class="text-muted my-3">如果你访问的是迁移之前的地址，点击上面的菜单，还可以找到相应的文章</p>
+
+                <p>{{ $exception->getMessage() }}</p>
+            </center>
         </div>
     </div>
 </div>
