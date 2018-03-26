e0dda46071e12bf21fdbc89943583e329422dece
Bro Qiang <broqiang@qq.com>
2018-03-27 01:49:41 +0800

    修改 404 模板

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,21 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="card">
+    <div class="card text-muted">
+        <div class="card-header bg-transparent">
+            <h1 class="text-danger text-center"> 404 !</h1>
+            <h5 class="card-title text-center my-3">访问的地址不存在！</h5>
+        </div>
         <div class="card-body">
-            <center>
-                <h1 class="text-warning"> 404 </h1>
-
-                <div class="dropdown-divider"></div>
-                
-                <h3 class="text-muted">
-                     访问的地址不存在 ! 
-                </h3>
-
-                <p class="text-muted my-3">如果你访问的是迁移之前的地址，点击上面的菜单，还可以找到相应的文章</p>
-
-                <p>{{ $exception->getMessage() }}</p>
-            </center>
+            <p class="text-muted my-3">如果你访问的是迁移之前的地址，点击上面的菜单，还可以找到相应的文章</p>
         </div>
     </div>
 </div>
