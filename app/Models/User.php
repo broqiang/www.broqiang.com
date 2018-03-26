27797ea7dd4115d82d82b776a537fb341143d5d1
Bro Qiang <broqiang@qq.com>
2018-03-27 01:43:54 +0800

    修复后台权限控制

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,15 +10,10 @@ use Illuminate\Support\Facades\Auth;
 class User extends Authenticatable
 {
     use Notifiable {
-        notify as laravelNotify;}function notify($instance)
-    {
-        // 如果要通知的人是当前用户，就不必通知了！
-        if ($this->id == Auth::id()) {
-            return;
-        }
-        $this->increment('notification_count');
-        $this->laravelNotify($instance);
+        notify as laravelNotify;
     }
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -38,7 +33,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    function posts()
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
+
+    public function posts()
     {
         return $this->hasMany(Post::class);
     }
@@ -47,7 +52,7 @@ class User extends Authenticatable
      * 用户关注的文章
      * @return [type] [description]
      */
-    function follows()
+    public function follows()
     {
         return $this->belongsToMany(Post::class, 'follows', 'user_id', 'post_id')
             ->orderBy('posts.created_at', 'desc')
@@ -55,28 +60,33 @@ class User extends Authenticatable
             ->withPivot('created_at');
     }
 
-    function followsAll()
+    public function followsAll()
     {
         return $this->belongsToMany(Post::class, 'follows', 'user_id', 'post_id')
             ->orderBy('posts.created_at', 'desc');
     }
 
-    function commentsPreview()
+    public function commentsPreview()
     {
         return $this->hasMany(Comment::class)
             ->limit(5)
             ->orderBy('comments.updated_at', 'desc');
     }
 
-    function comments()
+    public function comments()
     {
         return $this->hasMany(Comment::class)->orderBy('comments.updated_at', 'desc');
     }
 
-    function markAsRead()
+    public function markAsRead()
     {
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    public function isSuperAdmin ()
+    {
+        return $this->is_admin;
     }
 }
